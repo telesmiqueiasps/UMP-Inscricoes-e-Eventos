b5f0699bd9762e73babc89a8d6f65e2b28d65f2c
Miqueias Teles <miqueiasteles9@gmail.com>
2026-08-04 00:16:29 -0300

    ajuste de deploy

--- backend/app/routers/inscricoes.py
+++ backend/app/routers/inscricoes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 import uuid
 
 from app.core.database import get_db
